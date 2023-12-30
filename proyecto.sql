5c61673027c8ed824a27cf43e9d055b6916035ce
Tecrato <pinchos792003@gmail.com>
2023-12-30 16:56:30 -0400

    Update proyecto.sql

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-12-2023 a las 04:08:16
+-- Tiempo de generación: 30-12-2023 a las 21:54:29
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
