18544cc2fd84d97c29c361662c40a152c912a5be
L4riUs <garnicaluis391@gmail.com>
2024-01-30 17:28:14 -0400

    base de datos actualizada

--- proyecto.sql
+++ proyecto.sql
@@ -1,324 +1,303 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-01-2024 a las 18:17:23
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: proyecto_2
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `proyecto`
+-- Table structure for table `categoria`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoria`
---
-
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
-  `descripcion` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `descripcion` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `clientes`
+-- Dumping data for table `categoria`
 --
 
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'bebida',NULL),(2,'empaquetados',NULL);
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clientes` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(500) NOT NULL,
   `cedula` varchar(45) NOT NULL,
   `apellido` varchar(45) NOT NULL,
   `documento` varchar(1) NOT NULL,
   `direccion` varchar(255) NOT NULL,
-  `telefono` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `telefono` varchar(15) NOT NULL,
+  `active` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `entradas`
+-- Dumping data for table `clientes`
 --
 
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Luis','12345','Vargas','V','frente a mi casa','0000000',0),(6,'Jose','25698863','Perez','V','Direccion tal','04126742231',1);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entradas`
+--
+
+DROP TABLE IF EXISTS `entradas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entradas` (
-  `id` int(11) NOT NULL,
-  `id_producto` int(11) NOT NULL,
-  `id_proveedor` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_producto` int NOT NULL,
+  `id_proveedor` int NOT NULL,
+  `cantidad` int NOT NULL,
   `fecha_compra` date NOT NULL,
   `fecha_vencimiento` date NOT NULL,
   `precio_compra` float NOT NULL,
-  `existencia` int(11) NOT NULL DEFAULT 0,
-  `active` tinyint(1) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `existencia` int NOT NULL DEFAULT '0',
+  `active` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `id_producto` (`id_producto`),
+  KEY `id_proveedor` (`id_proveedor`),
+  CONSTRAINT `entradas_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
+  CONSTRAINT `entradas_ibfk_2` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `factura`
+-- Dumping data for table `entradas`
 --
 
+LOCK TABLES `entradas` WRITE;
+/*!40000 ALTER TABLE `entradas` DISABLE KEYS */;
+INSERT INTO `entradas` VALUES (1,1,7,10,'2023-12-31','2024-01-27',20,0,1),(2,17,7,10,'2023-12-31','2024-02-10',20,0,1),(3,17,1,10,'2023-12-31','2024-02-10',25,0,1),(4,19,7,25,'2023-12-31','2024-02-10',10,8,1),(5,19,7,10,'2023-12-31','2024-02-10',26,10,1),(6,16,7,8,'2024-01-02','2024-02-10',20,8,1),(7,21,7,88,'2023-12-31','2024-02-10',20,88,1);
+/*!40000 ALTER TABLE `entradas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `factura` (
-  `id` int(11) NOT NULL,
-  `id_registro_ventas` int(11) NOT NULL,
-  `id_productos` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `coste_producto_total` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_registro_ventas` int NOT NULL,
+  `id_productos` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `coste_producto_total` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_productos_has_registro_ventas_registro_ventas1_idx` (`id_registro_ventas`),
+  KEY `fk_productos_has_registro_ventas_productos1_idx` (`id_productos`),
+  CONSTRAINT `fk_productos_has_registro_ventas_productos1` FOREIGN KEY (`id_productos`) REFERENCES `productos` (`id`),
+  CONSTRAINT `fk_productos_has_registro_ventas_registro_ventas1` FOREIGN KEY (`id_registro_ventas`) REFERENCES `registro_ventas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Dumping data for table `factura`
 --
 
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+INSERT INTO `factura` VALUES (1,38,1,1,20),(2,39,1,5,100),(3,40,1,2,40),(5,41,1,2,40),(6,42,17,2,20),(8,43,17,2,20),(10,44,19,5,50),(11,44,17,1,10),(12,45,19,5,50),(13,46,17,2,20),(14,46,19,5,50),(15,47,17,1,10),(16,47,19,2,20),(17,48,17,12,120);
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `id_unidad` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_categoria` int NOT NULL,
+  `id_unidad` int NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `marca` varchar(100) NOT NULL,
   `imagen` varchar(500) NOT NULL DEFAULT 'banner_productos.png',
-  `stock_min` int(11) NOT NULL,
-  `stock_max` int(11) NOT NULL,
-  `precio_venta` float NOT NULL DEFAULT 0,
-  `IVA` tinyint(4) NOT NULL,
-  `active` tinyint(1) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `stock_min` int NOT NULL,
+  `stock_max` int NOT NULL,
+  `precio_venta` float NOT NULL DEFAULT '0',
+  `IVA` tinyint NOT NULL,
+  `active` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`),
+  KEY `id_categoria_idx` (`id_categoria`),
+  KEY `id_stock_max_min_idx` (`id_unidad`),
+  CONSTRAINT `id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`),
+  CONSTRAINT `id_stock_max_min` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `proveedores`
+-- Dumping data for table `productos`
 --
 
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,1,1,'harina pan','pan','banner_productos.png',10,20,20,0,0),(16,2,1,'Azucar','Azucar refinada blanca 500g','producto_Azucar_5e5294ee-d7d2-424d-ac2e-5802bbad41ab.jpeg',5,15,10,0,0),(17,1,1,'Azu','marquita','banner_productos.png',5,15,10,0,1),(18,2,1,'Pan','Polar','producto_Pan_7b37d828-2d86-40c5-b54a-9424c5dcc288.jpeg',5,15,10,0,0),(19,1,1,'Panpapa','mary','banner_productos.png',5,15,10,0,1),(20,1,1,'Jose','ujh','producto_Jose_2551fe44-3bc1-476e-b084-e7ff84eb8600.jpeg',4,4567,56,0,0),(21,2,1,'Corto','ujh','producto_Corto_2551fe44-3bc1-476e-b084-e7ff84eb8600.jpeg',4,4567,56,0,1),(22,2,1,'Abduzcan','ujh','producto_Abduzcan_5e5294ee-d7d2-424d-ac2e-5802bbad41ab.jpeg',4,4567,56,0,1),(23,2,1,'Arroz','BLUE','producto_Arroz_5ce0e0f8-46df-4654-b37b-7d7f40d9bc6a.jpeg',5,15,20,1,0),(24,1,1,'hola','ujh','producto_hola_5ce0e0f8-46df-4654-b37b-7d7f40d9bc6a.jpeg',4,4567,56,0,0),(29,1,1,'queso','Churuguara','producto_queso_2c51307c-9d9f-41fb-9419-1e61a44891f0.jpeg',5,15,20,1,1),(30,2,1,'doritos','mary','producto_doritos_bc7793c2-20fe-469c-b92c-7852f176f968.jpeg',5,15,10,1,0);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `proveedores` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `razon_social` varchar(50) NOT NULL DEFAULT 'natural',
   `rif` varchar(15) NOT NULL,
   `telefono` varchar(11) NOT NULL,
   `correo` varchar(50) NOT NULL,
   `direccion` varchar(45) NOT NULL,
-  `active` tinyint(1) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `active` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `registro_ventas`
+-- Dumping data for table `proveedores`
 --
 
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` VALUES (1,'tyrty','nn','j-00000000','1231231231','nose@gmail.com','54764576',1),(7,'montecarmelo','Montecarmelo','j-00000000','0000000','garnicaluis391@gmail.com','scacac',1);
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registro_ventas`
+--
+
+DROP TABLE IF EXISTS `registro_ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `registro_ventas` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `monto_final` float NOT NULL,
   `metodo_pago` varchar(50) NOT NULL,
-  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
-  `id_cliente` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `IVA` int(11) NOT NULL DEFAULT 0,
-  `active` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_cliente` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `IVA` int NOT NULL DEFAULT '0',
+  `active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `id_cliente_idx` (`id_cliente`),
+  KEY `id_usuario_idx` (`id_usuario`),
+  CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
+  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `unidades`
+-- Dumping data for table `registro_ventas`
 --
 
+LOCK TABLES `registro_ventas` WRITE;
+/*!40000 ALTER TABLE `registro_ventas` DISABLE KEYS */;
+INSERT INTO `registro_ventas` VALUES (38,20,'Efectivo','2024-01-27 09:08:56',1,1,0,0),(39,100,'Pago Movil','2024-01-28 14:37:04',1,1,0,1),(40,180,'Punto de Venta','2024-01-28 15:23:57',1,1,0,1),(41,40,'Pago Movil','2024-01-28 15:30:30',1,1,0,1),(42,40,'Pago Movil','2024-01-28 15:33:22',1,1,0,1),(43,40,'Pago Movil','2024-01-28 15:35:02',1,1,0,1),(44,60,'Punto de Venta','2024-01-28 15:38:11',1,1,0,1),(45,50,'Punto de Venta','2024-01-28 18:41:00',1,1,0,1),(46,70,'Pago Movil','2024-01-28 18:43:50',6,1,0,1),(47,30,'Punto de Venta','2024-01-29 18:31:07',1,1,0,0),(48,120,'Punto de Venta','2024-01-29 20:27:38',6,2,0,1);
+/*!40000 ALTER TABLE `registro_ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unidades`
+--
+
+DROP TABLE IF EXISTS `unidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `unidades` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Dumping data for table `unidades`
 --
 
+LOCK TABLES `unidades` WRITE;
+/*!40000 ALTER TABLE `unidades` DISABLE KEYS */;
+INSERT INTO `unidades` VALUES (1,'g');
+/*!40000 ALTER TABLE `unidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `correo` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `rol` int(11) NOT NULL DEFAULT 3
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `rol` int NOT NULL DEFAULT '3',
+  `active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tablas volcadas
+-- Dumping data for table `usuarios`
 --
 
---
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Edouard','nose@gmail.com','12345',1,1),(2,'nose','pinchos792003@gmail.com','asd',2,1),(3,'Ale','garnicaluis391@gmail.com','123',2,1);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indices de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `entradas`
---
-ALTER TABLE `entradas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_producto` (`id_producto`),
-  ADD KEY `id_proveedor` (`id_proveedor`);
-
---
--- Indices de la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_productos_has_registro_ventas_registro_ventas1_idx` (`id_registro_ventas`),
-  ADD KEY `fk_productos_has_registro_ventas_productos1_idx` (`id_productos`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`),
-  ADD KEY `id_categoria_idx` (`id_categoria`),
-  ADD KEY `id_stock_max_min_idx` (`id_unidad`);
-
---
--- Indices de la tabla `proveedores`
---
-ALTER TABLE `proveedores`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `registro_ventas`
---
-ALTER TABLE `registro_ventas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_cliente_idx` (`id_cliente`),
-  ADD KEY `id_usuario_idx` (`id_usuario`);
-
---
--- Indices de la tabla `unidades`
---
-ALTER TABLE `unidades`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `entradas`
---
-ALTER TABLE `entradas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `factura`
---
-ALTER TABLE `factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `proveedores`
---
-ALTER TABLE `proveedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `registro_ventas`
---
-ALTER TABLE `registro_ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `unidades`
---
-ALTER TABLE `unidades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `entradas`
---
-ALTER TABLE `entradas`
-  ADD CONSTRAINT `entradas_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
-  ADD CONSTRAINT `entradas_ibfk_2` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id`);
-
---
--- Filtros para la tabla `factura`
---
-ALTER TABLE `factura`
-  ADD CONSTRAINT `fk_productos_has_registro_ventas_productos1` FOREIGN KEY (`id_productos`) REFERENCES `productos` (`id`),
-  ADD CONSTRAINT `fk_productos_has_registro_ventas_registro_ventas1` FOREIGN KEY (`id_registro_ventas`) REFERENCES `registro_ventas` (`id`);
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`),
-  ADD CONSTRAINT `id_stock_max_min` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id`);
-
---
--- Filtros para la tabla `registro_ventas`
---
-ALTER TABLE `registro_ventas`
-  ADD CONSTRAINT `id_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
-  ADD CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-30 17:26:06
