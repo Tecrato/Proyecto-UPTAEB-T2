ede9dec08b8ed2eb23b7359475bd274cb2fd6d8e
Juanxrpana <86117653+Juanxrpana@users.noreply.github.com>
2024-06-16 01:24:42 -0400

    si
    modelo para las vistas, estaba funcionando ;D
    
    Signed-off-by: Juanxrpana <86117653+Juanxrpana@users.noreply.github.com>

---
+++ Model/Estadisticas.php
@@ -0,0 +1,34 @@
+<?php
+    class Estadisticas extends DB {
+
+        function __construct(){
+            DB::__construct();
+        }
+        function ratio_ventas(){
+            $query = $this->conn->prepare('SELECT * FROM ratio_ventas');
+            $query->execute();
+            return $query->fetchAll();
+        }
+        function total_productos_categoria(){
+            $query = $this->conn->prepare('SELECT * FROM total_productos_categoria');
+            $query->execute();
+            return $query->fetchAll();
+        }
+        function total_stock_categoria(){
+            $query = $this->conn->prepare('SELECT * FROM total_stock_categoria');
+            $query->execute();
+            return $query->fetchAll();
+        }
+        function max_ventas(){
+            $query = $this->conn->prepare('SELECT * FROM max_ventas');
+            $query->execute();
+            return $query->fetchAll();
+        }
+        function min_ventas(){
+            $query = $this->conn->prepare('SELECT * FROM min_ventas');
+            $query->execute();
+            return $query->fetchAll();
+        }
+    }
+?>
+
