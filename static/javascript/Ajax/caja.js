c7b6529cceac238d029d0795a5aab14cb22a4d16
L4riUs <garnicaluis391@gmail.com>
2024-06-21 18:48:49 -0400

    Update caja.js

--- static/javascript/Ajax/caja.js
+++ static/javascript/Ajax/caja.js
@@ -1,3 +1,57 @@
+const cargarCajas = () => {
+    $.ajax({
+        url: "Controller/funcs_ajax/search.php",
+        type: "GET",
+        data: {randomnautica : "caja"},
+        success: function (response) {
+            let json = JSON.parse(response);
+            let template = ""
+    
+            json.lista.forEach(element => {
+                template += `<tr>
+                                <td>${element.id}</td>
+                                <td>${element.id_usuario}</td>
+                                <td>${fecha(element.fecha)}</td>
+                                <td>${element.monto_inicial}</td>
+                                <td>${hora(element.fecha)}</td>
+                                <td>${element.fecha_cierre == null ? "00:00" : element.fecha_cierre}</td>
+                                <td>${element.total_ventas}</td>
+                                <td>${element.monto_credito}</td>
+                                <td>${element.monto_final}</td>
+                                <td>
+                                    <div class="activeGood uk-border-rounded" style="padding: 5px;">ABIERTA</div>
+                                </td>
+                                <td>
+                                    <a href="#cierre-caja" uk-toggle class="uk-button uk-button-default">CERRAR CAJA</a>
+                                </td>
+                            </tr>`
+            })
+    
+            $("#tbody_caja").html(template)
+           
+        }
+    })
+}
+cargarCajas()
+
+const hora = (f) => {
+    const fecha = new Date(f);
+    const horas = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const periodo = horas >= 12 ? "PM" : "AM";
+    const horas12 = horas % 12 || 12;
+    const horaFormateada = `${horas12}:${minutos < 10 ? `0${minutos}` : minutos} ${periodo}`;
+return horaFormateada
+}
+const fecha = (f) => {
+const fecha = new Date(f);
+const dia = fecha.getDate();
+const mes = fecha.getMonth() + 1;
+const anio = fecha.getFullYear();
+const fechaFormateada = `${dia}/${mes}/${anio}`;
+return fechaFormateada
+}
+
 let formCaja =  document.getElementById("FormCaja");
 // console.log(formCaja);
 formCaja.addEventListener("submit", (e) => {
@@ -13,6 +67,7 @@ formCaja.addEventListener("submit", (e) => {
         contentType: false,
         processData: false,
         success: function (response) {
+            cargarCajas()
             console.log(response);
             UIkit.notification.closeAll();
           UIkit.notification({
@@ -24,39 +79,6 @@ formCaja.addEventListener("submit", (e) => {
     })
 
 
-})
-
-
-
-$.ajax({
-    url: "Controller/funcs_ajax/search.php",
-    type: "GET",
-    data: {randomnautica : "caja"},
-    success: function (response) {
-        let json = JSON.parse(response);
-        let template = ""
-
-        json.lista.forEach(element => {
-            template += `<tr>
-                            <td>${element.id}</td>
-                            <td>${element.id_usuario}</td>
-                            <td>${element.fecha}</td>
-                            <td>${element.monto_inicial}</td>
-                            <td>${element.fecha}</td>
-                            <td>${element.fecha}</td>
-                            <td>15</td>
-                            <td>600.00</td>
-                            <td>1500.00</td>
-                            <td>
-                                <div class="activeGood uk-border-rounded" style="padding: 5px;">ABIERTA</div>
-                            </td>
-                            <td>
-                                <a href="#cierre-caja" uk-toggle class="uk-button uk-button-default">CERRAR CAJA</a>
-                            </td>
-                        </tr>`
-        })
-       
-    }
 })
 
 
