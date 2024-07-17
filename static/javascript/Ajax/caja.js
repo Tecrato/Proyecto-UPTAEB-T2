333d2bc7bbe5ea94e110ea309b3aaa4c582b53cd
Tecrato <pinchos792003@gmail.com>
2024-07-17 00:09:35 -0400

    calma, regresa

--- static/javascript/Ajax/caja.js
+++ static/javascript/Ajax/caja.js
@@ -83,12 +83,11 @@ function cargarCajas(page) {
             })
 
             $("#tbody_caja").html(template)
-            if (parseInt(session_user_rol_num) = 1 ) {
-                $(".cerrarCaja").removeClass("invisible")
-                $(".btn_agg_caja").removeClass("invisible")
+            if (parseInt(session_user_rol_num) <= 2 ) {
+                $(".date_caja").removeClass("invisible")
                 return
               } else {
-                PermisosG(".cerrarCaja", null, "caja", "", "G")
+                PermisosG(null, null, "caja", ".date_caja", "R")
               }
 
             let cerrarCaja = document.querySelectorAll(".cerrarCaja");
