2fe353e228d5f624d229590cbbcdbba388c1fc33
L4riUs <garnicaluis391@gmail.com>
2024-10-22 12:36:40 -0400

    filtros, otra vez

--- src/static/javascript/Ajax/supplier.js
+++ src/static/javascript/Ajax/supplier.js
@@ -270,4 +270,87 @@ function cardProv(page) {
 
 document.querySelector(".preloader_container").remove()
 cardProv(0)
+
+let searchFilter = document.querySelector("#SearchSupplier")
+
+searchFilter.addEventListener("keyup", () => {
+  let value = searchFilter.value
+  $.ajax({
+    url: "api_search",
+    type: "GET",
+    data: { randomnautica: "proveedores", like: value, active: 1 },
+    success: function (response) {
+      let json = JSON.parse(response);
+      let template = "";
+      if (json.lista.length == 0) {
+        marcaAgua()
+        
+      } else {
+        json.lista.forEach((element) => {
+        template += `<div>
+                          <div class="target_supplier uk-card uk-card-default uk-flex uk-padding-small uk-light uk-border-rounded" style=" background-color: #333;">
+                              <div>
+                                  <div class="img_proveedor_container uk-border-rounded">
+                                      <img src="static/images/logo_proveedor.png" alt="" width="80px" />
+                                      <h5 class="uk-margin-remove-left uk-margin-remove-right uk-margin-small-top uk-text-center">${element.razon_social}</h5>
+                                  </div>
+                              </div>
+                      
+                              <div style="width: 180px;">
+                                  <div class="uk-flex uk-flex-middle uk-flex-between uk-margin-small-bottom">
+                                      <h4 class="uk-margin-remove-bottom uk-margin-right uk-text-center">DATOS</h4>
+                                      <div>
+                                          <a href="#modal-modificar" id="${element.id}" uk-tooltip="title:Editar; delay: 500"
+                                              class="uk-icon-button uk-margin-small-right edit_prov" type="button"
+                                              style="border: none; cursor: pointer">
+                                              <span uk-icon="icon: file-edit"></span>
+                                          </a>
+                                          <a href="#eliminar_supplier" uk-toggle id="${element.id}" class="uk-icon-button uk-margin-small-right delete_prov"
+                                              uk-tooltip="title:Eliminar; delay: 500" type="button"
+                                              style="border: none; cursor: pointer" type="button">
+                                              <span uk-icon="icon: trash"></span>
+                                          </a>
+                                      </div>
+                                  </div>
+                      
+                                  <hr class="uk-margin-bottom uk-margin-remove-top hr_supplier" />
+                      
+                                  <div class="Container-details-suppliers" style="width: 200px;">
+                                      <div class="uk-flex">
+                                          <h6 class="uk-margin-small">Nombre</h6>
+                                          <p class="uk-margin-small uk-margin-small-left uk-margin-remove-top uk-text-meta">
+                                          ${element.nombre}
+                                          </p>
+                                      </div>
+                                      <div class="uk-flex">
+                                          <h6 class="uk-margin-small">RIF</h6>
+                                          <p class="uk-margin-small uk-margin-small-left uk-margin-remove-top uk-text-meta">
+                                          ${element.rif}
+                                          </p>
+                                      </div>
+                                      <div class="uk-flex">
+                                          <h6 class="uk-margin-small">Direccion</h6>
+                                          <p class="uk-margin-small uk-margin-small-left uk-margin-remove-top uk-text-break uk-text-meta"
+                                              style="width: 185px">
+                                              ${element.direccion}
+                                          </p>
+                                      </div>
+                                      <div class="uk-flex">
+                                          <h6 class="uk-margin-small">Telefono</h6>
+                                          <p class="uk-margin-small uk-margin-small-left uk-margin-remove-top uk-text-meta">
+                                          ${element.telefono}
+                                          </p>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>`;
+      });
+      }
+       
+      $(".cont_prov_cards").html(template)
+      marcaAgua()
+    }
+  })
+})
 insertANDupdateCLient_proveedor('.form_prov', "#tlfn_pais", cardProv, "proveedor")
