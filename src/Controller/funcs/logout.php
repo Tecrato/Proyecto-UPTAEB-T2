1fb42c4bba6d0eb6aa6e14f4880110677f387f94
Tecrato <pinchos792003@gmail.com>
2024-10-30 16:04:14 -0400

    Update logout.php

--- src/Controller/funcs/logout.php
+++ src/Controller/funcs/logout.php
@@ -8,12 +8,15 @@
     use Shtechnologyx\Pt3\Model\Usuario;
     use Shtechnologyx\Pt3\Model\Bitacora;
 
+    try {
+        $usu = new Usuario($_SESSION['user_id']);
+        $usu->logout();
+    
+        $clase2 = new Bitacora(null,$_SESSION['user_id'],"Usuarios","Logout","Usuario ".$_SESSION['user_name']." des-logueado");
+        $clase2->agregar();
+    } catch (Exception $e) {
 
-    $usu = new Usuario($_SESSION['user_id']);
-    $usu->logout();
-
-    $clase2 = new Bitacora(null,$_SESSION['user_id'],"Usuarios","Logout","Usuario ".$_SESSION['user_name']." des-logueado");
-    $clase2->agregar();
+    }
 
     session_destroy();
     header("Location:Login"); // Y pal login
