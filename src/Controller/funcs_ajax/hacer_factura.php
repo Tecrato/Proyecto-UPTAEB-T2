eea7acc61d0d8eeff2728b0ed0f7545f9eac99f3
Tecrato <pinchos792003@gmail.com>
2024-10-30 18:38:31 -0400

    Ya factura denuevo

--- src/Controller/funcs_ajax/hacer_factura.php
+++ src/Controller/funcs_ajax/hacer_factura.php
@@ -15,14 +15,21 @@
 
 	$otra_clase_mas = new Caja(id_usuario:$_SESSION['user_id'], estado:0);
 	// $ultima_caja = $otra_clase_mas->buscar_ultima();
-	$ultima_caja = $otra_clase_mas->search();
+	$ultima_caja = $otra_clase_mas->search()[0];
 	print_r($ultima_caja);
 	if ($ultima_caja == NULL or count($ultima_caja) == 0) {
 		echo json_encode(['status' => 'error','error'=>'Caja Error']);
         exit(0);
         die();
 	}
-	$clase2 = new Registro_ventas(null,$var->monto_final,$var->id_cliente, $ultima_caja['0'],$var->IVA,$var->active);
+	$clase2 = new Registro_ventas(
+		id:null,
+		monto_final:$var->monto_final,
+		id_cliente:$var->id_cliente, 
+		id_caja:$ultima_caja['id'],
+		IVA:$var->IVA,
+		active:1,
+	);
 	$result = $clase2->agregar_venta($var->detalles, $var->pagos, $var->credito, $var->fecha_inicio_credito, $var->fecha_cierre_credito,$var->monto_dolar);
 
 	$clase3 = new Bitacora(null,$_SESSION['user_id'],"Caja","Cerrar","Caja cerrada");
